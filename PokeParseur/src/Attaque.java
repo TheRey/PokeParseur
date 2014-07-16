add86f629fa35314e7a89161daba9eff7e4becb3
TheoGauchoux <theo.gau@gmail.com>
2014-07-16 21:10:38 +0200

    L'appli a attrapée des pokémons !

---
+++ PokeParseur/src/Attaque.java
@@ -0,0 +1,12 @@
+
+public class Attaque
+{
+	int id;
+	int niveau;
+	
+	public Attaque(int id, int niveau)
+	{
+		this.id = id;
+		this.niveau = niveau;
+	}
+}
--- PokeParseur/src/Pokemon.java
+++ PokeParseur/src/Pokemon.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+
 
 public class Pokemon
 {
-
+	int numero;
+	String nom;
+	String famille;
+	String[] genre;
+	int tauxCapture;
+	String[] experience_experienceMax;
+	String[] types;
+	int idPreEvolution;
+	String[] evolution;
+	int nombrePasEclosion;
+	String[] groupesOeuf;
+	int nombreFormes;
+	String[] taille_poids;
+	String[] statsBase;
+	String[] statsEVs;
+	String[] idCapSpe;
+	String description;
+	ArrayList<Attaque> attaquesParNiveau;
+	ArrayList attaquesParCapsules;
+	ArrayList attaquesQuandIlsBaisent;
+	ArrayList attaqueMaitreCapacite;
+	
+	public Pokemon(int id)
+	{
+		this.numero = id;
+	}
 }
